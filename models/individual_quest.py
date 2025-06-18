b18bb8ed13c6ec688e8660ce1552390c9d62b8d3
Chenhao Huang <goldenhch@gmail.com>
2025-06-18 15:23:34 -0600

    Updated individual_quest model and added LTG creation

--- models/individual_quest.py
+++ models/individual_quest.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Optional, Literal
+from models.rubric import Rubric
 
 class IndividualQuest(BaseModel):
     quest_id: str  # Partition Key
     description: str
-    grade: str = Field(description="Grade provided by the grader")
-    feedback: str = Field(description="Feedback provided by the grader")
+    grade: Optional[str] = Field(description="Grade provided by the grader")
+    feedback: Optional[str] = Field(description="Feedback provided by the grader")
     skills: str = Field(description="Skills the student will practice through this quest")
+    Week: int = Field(description="Week the student will work on this quest")
+    instructions: str = Field(description="Detailed instructions for completing the quest")
+    rubric: Rubric = Field(description="Grading criteria and expectations for the quest")
+    status: Literal["not_started", "in_progress", "completed"] = Field(description="Status of the quest")
     created_at: str
     due_date: str  
       
