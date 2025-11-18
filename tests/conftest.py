ab4af19a2620af7e6967b98d2b0088a5b79ce2e2
JasonXing1234 <113735814+JasonXing1234@users.noreply.github.com>
2025-11-18 01:42:36 -0600

    Add openai.types.shared and boto3 mocks for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,9 @@ from unittest.mock import MagicMock
 
 os.environ['OPENAI_API_KEY'] = 'test-key-for-ci'
 os.environ['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'test-secret-key')
+os.environ['AWS_ACCESS_KEY_ID'] = 'test-aws-key'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'test-aws-secret'
+os.environ['AWS_REGION'] = 'us-east-2'
 
 mock_openai_module = MagicMock()
 mock_openai_client = MagicMock()
@@ -12,6 +15,8 @@ mock_openai_module.api_key = 'test-key'
 
 sys.modules['openai'] = mock_openai_module
 sys.modules['openai.types'] = MagicMock()
+sys.modules['openai.types.shared'] = MagicMock()
+sys.modules['openai.types.shared.reasoning'] = MagicMock()
 sys.modules['openai.types.shared_params'] = MagicMock()
 sys.modules['openai.types.shared_params.response_format_json_schema'] = MagicMock()
 sys.modules['openai.beta'] = MagicMock()
@@ -19,3 +24,7 @@ sys.modules['openai.beta.assistants'] = MagicMock()
 sys.modules['openai.beta.threads'] = MagicMock()
 sys.modules['openai.beta.threads.messages'] = MagicMock()
 sys.modules['openai.beta.threads.runs'] = MagicMock()
+
+sys.modules['boto3'] = MagicMock()
+sys.modules['botocore'] = MagicMock()
+sys.modules['botocore.exceptions'] = MagicMock()
