84dd804482ad12d5383f1df8ac518f0ec7586ed7
JasonXing1234 <113735814+JasonXing1234@users.noreply.github.com>
2025-11-18 05:53:03 -0600

    Update routes.py

--- routes/auth/routes.py
+++ routes/auth/routes.py
@@ -14,7 +14,7 @@ session_dao = SessionDAO()
 student_dao = StudentDAO()
 teacher_dao = TeacherDAO()
 conversation_service = ConversationService()
-#sdf1234567890123
+#sdf12345678901234
 @auth_bp.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
