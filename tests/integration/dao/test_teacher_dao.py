58f72dc0fa2f41ab7fc9e989feff0d6cf07673b1
Joshua Job <joshuasjob@hotmail.com>
2026-04-30 14:54:06 -0600

    Update testing

--- tests/integration/dao/test_teacher_dao.py
+++ tests/integration/dao/test_teacher_dao.py
@@ -43,6 +43,23 @@ def test_update_teacher(supabase_required):
 
 
 @pytest.mark.integration
+def test_update_canvas_credentials(supabase_required):
+    dao = TeacherDAO()
+    try:
+        dao.add_teacher(_teacher())
+        dao.update_canvas_credentials(_ID, "https://canvas.example.com", "key123")
+        result = dao.get_teacher_by_id(_ID)
+        assert result["canvas_api_url"] == "https://canvas.example.com"
+        assert result["canvas_api_key"] == "key123"
+        dao.clear_canvas_credentials(_ID)
+        result2 = dao.get_teacher_by_id(_ID)
+        assert result2["canvas_api_url"] is None
+        assert result2["canvas_api_key"] is None
+    finally:
+        dao.delete_teacher(_ID)
+
+
+@pytest.mark.integration
 def test_delete_teacher(supabase_required):
     dao = TeacherDAO()
     dao.add_teacher(_teacher())
